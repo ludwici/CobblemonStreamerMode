122392a8a023b1dc3111e661982283fc91ce8863
ludwici <gammaclone@gmail.com>
2026-09-20 20:26:40 +0300

    Change modid

--- src/main/java/com/ludwici/cobblemonstreamermode/CobblemonStreamerMode.java
+++ src/main/java/com/ludwici/cobblemonstreamermode/CobblemonStreamerMode.java
@@ -12,7 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CobblemonStreamerMode implements ModInitializer {
-	public static final String MODID = "cobblemon-streamer-mode";
+	public static final String MODID = "cobblemon_streamer_mode";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
